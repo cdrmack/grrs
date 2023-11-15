9bdcf75a66045002b614df6d06e32d763158a2b1
cdrmack <git.pynvt@passmail.net>
2023-11-15 13:04:15 +0100

    store arguments inside the Cli struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("missing pattern to search for");
+    let path = std::env::args().nth(2).expect("missing path");
+
+    let args = Cli {
+	pattern: pattern,
+	path: std::path::PathBuf::from(path),
+    };
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
